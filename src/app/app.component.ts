4b64b48e3999e8e96e48ebe597fb25d60359fd15
Umashankar Varma M <umashankar_varma_m@dell.com>
2022-11-23 17:27:41 +0530

    ngFor cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {title: "Neat Tree", username:"@nature", imgurl: "assets/tree.jpeg", content:"Saw a cool tree during my hike today."},
+    {title: "Snowy Mountain", username:"@mountainlover", imgurl: "assets/mountain.jpeg", content:"Mountain scenes during current fall."},
+    {title: "My lovely bike", username:"@bikerider", imgurl: "assets/biking.jpeg", content:"My new bike."},
+  ];
 }
